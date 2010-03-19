9394ceab750eb75fe3bdf83eecf23cfc9d07507d
Mihail Atanassov <saeko.bjagai@gmail.com>
2010-03-19 19:15:34 +0100

    JPG files replaced with larger versions (900 x 1440 Pixel), closes #75

--- de.uni_koeln.ub.drc/src/tests/de/uni_koeln/ub/drc/reader/TestPositionParser.java
+++ de.uni_koeln.ub.drc/src/tests/de/uni_koeln/ub/drc/reader/TestPositionParser.java
@@ -46,8 +46,8 @@ public final class TestPositionParser {
   @Test
   public void point() {
     Line testLine = paragraphs.get(1).getLines().get(0);
-    Point scaledStart = testLine.getStartPointScaled(600, 960);
-    Assert.assertEquals(new Point(77, 348), scaledStart);
+    Point scaledStart = testLine.getStartPointScaled(900, 1440);
+    Assert.assertEquals(new Point(116, 522), scaledStart);
     Assert.assertTrue(testLine.getText().toString().startsWith("CatechiSmus"));
   }
 
