17c10f258cda4708f437f0ee3b0ee9881d8ab089
Mihail Atanassov <saeko.bjagai@gmail.com>
2010-03-26 18:56:43 +0100

    Implemented method for extraction of scaled font size, cleaned up documentation, closes #23

--- de.uni_koeln.ub.drc/src/java/de/uni_koeln/ub/drc/reader/PositionParser.java
+++ de.uni_koeln.ub.drc/src/java/de/uni_koeln/ub/drc/reader/PositionParser.java
@@ -14,53 +14,50 @@ import java.util.Scanner;
 
 /**
  * Parses a PDF to paragraphs and lines.
- * 
  * @author Mihail Atanassov <saeko.bjagai@gmail.com>
  */
 public final class PositionParser {
 
-	private PositionParser() {
-		// Static utility class
-	}
+  private PositionParser() {
+  // Static utility class
+  }
 
-	/**
-	 * @param pdf
-	 *            The PDF document to parse
-	 * @return The paragraphs parsed from the PDF
-	 */
-	public static List<Paragraph> parse(final String pdf) {
-		PdfContentReader reader = new PdfContentReader(pdf);
-		List<Paragraph> paragraphs = new ArrayList<Paragraph>();
-		Scanner s = new Scanner(reader.getPdfContent());
+  /**
+   * @param pdf The PDF document to parse
+   * @return The paragraphs parsed from the PDF
+   */
+  public static List<Paragraph> parse(final String pdf) {
+    PdfContentReader reader = new PdfContentReader(pdf);
+    List<Paragraph> paragraphs = new ArrayList<Paragraph>();
+    Scanner s = new Scanner(reader.getPdfContent());
 
-		float fontSize = -1;
-		Paragraph p = null;
-		Line currentLine = null;
+    float fontSize = -1;
+    Paragraph p = null;
+    Line currentLine = null;
 
-		while (s.hasNextLine()) {
-			String line = s.nextLine().trim();
-			if (line.equals("BT")) {
-				p = new Paragraph();
-				paragraphs.add(p);
-			}
+    while (s.hasNextLine()) {
+      String line = s.nextLine().trim();
+      if (line.equals("BT")) {
+        p = new Paragraph();
+        paragraphs.add(p);
+      }
 
-			if (line.endsWith("Tf")) {
-				fontSize = Float.parseFloat(line.split(" ")[1]);
-			}
+      if (line.endsWith("Tf")) {
+        fontSize = Float.parseFloat(line.split(" ")[1]);
+      }
 
-			if (line.endsWith("Tm")) {
-				String[] tokens = line.split(" ");
-				float x = Float.parseFloat(tokens[4]);
-				float y = Float.parseFloat(tokens[5]);
-				currentLine = new Line(reader.getPdfBox(), x, y, fontSize,
-						new StringBuilder());
-				p.addLine(currentLine);
-			}
-			if (currentLine != null && line.endsWith("Tj")) {
-				line = line.replaceAll("[\\(\\)]| Tj", "");
-				currentLine.getText().append(line);
-			}
-		}
-		return paragraphs;
-	}
+      if (line.endsWith("Tm")) {
+        String[] tokens = line.split(" ");
+        float x = Float.parseFloat(tokens[4]);
+        float y = Float.parseFloat(tokens[5]);
+        currentLine = new Line(reader.getPdfBox(), x, y, fontSize, new StringBuilder());
+        p.addLine(currentLine);
+      }
+      if (currentLine != null && line.endsWith("Tj")) {
+        line = line.replaceAll("[\\(\\)]| Tj", "");
+        currentLine.getText().append(line);
+      }
+    }
+    return paragraphs;
+  }
 }
