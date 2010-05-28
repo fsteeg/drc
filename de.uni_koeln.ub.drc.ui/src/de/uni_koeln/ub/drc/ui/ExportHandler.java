6167d07c499509067baa9a3fb77569a8f28662ab
Fabian Steeg <fsteeg@gmail.com>
2010-05-28 15:40:36 +0200

    Added binding for export, updated labels and tooltips; closes #38

--- de.uni_koeln.ub.drc.ui/src/de/uni_koeln/ub/drc/ui/ExportHandler.java
+++ de.uni_koeln.ub.drc.ui/src/de/uni_koeln/ub/drc/ui/ExportHandler.java
@@ -14,8 +14,10 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.model.application.ui.MContext;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.workbench.ui.IWorkbench;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -33,12 +35,19 @@ import de.uni_koeln.ub.drc.data.Page;
 public final class ExportHandler {
   private List<Page> pages;
 
+  /* This one is required for the binding */
+  @CanExecute boolean canExecute(@Named( IServiceConstants.ACTIVE_PART ) MContext context) {
+    pages = (List<Page>) context.getContext().get(IServiceConstants.SELECTION);
+    return pages != null;
+  }
+
+  /* This one is required for menu and tool bar items */
   @Inject public void setSelection(
       @Optional @Named( IServiceConstants.SELECTION ) final List<Page> pages) {
     this.pages = pages;
   }
-  @Execute
-  public void execute(final IWorkbench workbench) {
+
+  @Execute public void execute(final IWorkbench workbench) {
     Shell shell = Display.getCurrent().getActiveShell();
     if (pages == null) {
       MessageDialog.openError(shell, "No pages selected", "Please select pages to export");
@@ -51,8 +60,8 @@ public final class ExportHandler {
     if (location != null) {
       save(pages, location);
       String pagesString = pages.size() == 1 ? "page" : "pages";
-      MessageDialog.openInformation(shell, "Export", String.format("%s " + pagesString
-          + " exported to %s", pages.size(), location));
+      MessageDialog.openInformation(shell, "Export",
+          String.format("%s " + pagesString + " exported to %s", pages.size(), location));
     }
   }
 
@@ -72,8 +81,8 @@ public final class ExportHandler {
   private String concat(final List<Page> pages) {
     StringBuilder builder = new StringBuilder();
     for (Page page : pages) {
-      builder.append(page.id()).append("\n\n").append(page.toText().replace(" | ", "\n\n")).append(
-          "\n\n");
+      builder.append(page.id()).append("\n\n").append(page.toText().replace(" | ", "\n\n"))
+          .append("\n\n");
     }
     return builder.toString();
   }
