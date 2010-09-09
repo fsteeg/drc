e913976966fa9949089cf8498aacaa0fc065a6cc
Fabian Steeg <fsteeg@gmail.com>
2010-09-09 13:35:55 +0200

    Changed DB location for CI builds (closes #79)

--- de.uni_koeln.ub.drc/src/scala/de/uni_koeln/ub/drc/data/Index.scala
+++ de.uni_koeln.ub.drc/src/scala/de/uni_koeln/ub/drc/data/Index.scala
@@ -64,8 +64,11 @@ object Import extends Application {
     User.initialImport("users");
 }
 
+object LocalDb extends XmlDb("xmldb:exist://localhost:8080/exist/xmlrpc", "/db/", "drc/")
+object ServerDb extends XmlDb("xmldb:exist://hydra4.spinfo.uni-koeln.de:8080/exist/xmlrpc", "/db/", "drc/")
+
 object Index {
-    val Db = XmlDb("xmldb:exist://localhost:8888/exist/xmlrpc", "/db/", "drc/")
+    val Db = ServerDb
     lazy val lexicon: Set[String] =
       (Set() ++ scala.io.Source.fromInputStream(
           Index.getClass.getResourceAsStream("words.txt"))("ISO-8859-1").getLines()).map(
