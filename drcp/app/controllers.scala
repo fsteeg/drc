16749578504f83cd6691f4b876670869e8a02dfa
Fabian Steeg <fsteeg@gmail.com>
2011-01-19 18:25:31 +0100

    Tweaked portal look, added page info (#198)

--- drcp/app/controllers.scala
+++ drcp/app/controllers.scala
@@ -7,12 +7,19 @@ import play.mvc._
 
 object Application extends Controller {
 
-  val db = XmlDb("xmldb:exist://hydra1.spinfo.uni-koeln.de:8080/exist/xmlrpc", "db", "drc")
+  val server = "hydra1.spinfo.uni-koeln.de:8080"
+  val collection = "PPN345572629_0004"
+  val db = XmlDb("xmldb:exist://" + server + "/exist/xmlrpc", "db", "drc")
 
   def index = {
     val users = for (u <- db.getXml("users").get) yield User.fromXml(u)
     val (active, inactive) = users.sortBy(_.reputation).reverse.partition(_.reputation > 0)
-    Template(active, inactive)
+
+    val ids = db.getIds(collection).get.filter(_.endsWith(".xml"))
+    val pages = ids.take(2).map("http://" + server + "/exist/rest/db/drc/" + collection + "/" +
+      _.replace(".xml", ".jpg"))
+
+    Template(active, inactive, ids, pages)
   }
 
 }
