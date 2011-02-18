bb558e4cf621005e95be20bfabcec298274c47fd
Fabian Steeg <fsteeg@gmail.com>
2011-02-18 16:58:10 +0100

    Clean merge conflict in portal

--- drcp/test/BasicTest.java
+++ drcp/test/BasicTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.junit.*;
 import java.util.*;
 import play.test.*;
@@ -12,18 +11,3 @@ public class BasicTest extends UnitTest {
     }
 
 }
-=======
-import org.junit.*;
-import java.util.*;
-import play.test.*;
-import models.*;
-
-public class BasicTest extends UnitTest {
-
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
-
-}
->>>>>>> 7baf2ce04cb514e8cbd3abeb3dba87b5fb4114c9
