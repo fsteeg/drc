643b2465535c32468befc199b5833dd5ad764e5b
Fabian Steeg <fsteeg@gmail.com>
2011-05-06 15:26:53 +0200

    Basic content for portal

--- drcp/app/controllers.scala
+++ drcp/app/controllers.scala
@@ -12,23 +12,34 @@ object Application extends Controller {
   val port = 8080
   val db = XmlDb(server, port)
   val col = "drc"
+	  
+  lazy val u =
+	  (for (u <- db.getXml(col + "/users").get) yield User.fromXml(u))
+	  .sortBy(_.reputation).reverse.partition(_.reputation > 0)
 
   def index = {
-    val users = for (u <- db.getXml(col+"/users").get) yield User.fromXml(u)
-    val (active, inactive) = users.sortBy(_.reputation).reverse.partition(_.reputation > 0)
+	val top = u._1.take(5)
+    //val ids = db.getIds(col + "/PPN345572629_0004").get.filter(_.endsWith(".xml"))
+    //val pages = ids.take(5).map(imageLink(_))
+	val ids=List()
+    Template(top, ids/*, pages*/)
+  }
+  
+  def contact = { Template() }
+  def faq = { Template() }
+  def info = { Template() }
 
-    val ids = db.getIds(col+"/PPN345572629_0004").get.filter(_.endsWith(".xml"))
-    val pages = ids.take(5).map(imageLink(_))
-
-    Template(active, inactive, ids, pages)
+  def users = {
+	val (active, inactive) = u
+    Template(active, inactive)
   }
 
   private def imageLink(id: String) = "http://" + server + ":" + port + "/exist/rest/db/" + col + "/" +
-    (if(id.matches(".*?PPN345572629_0004-000[1-6].*?")) id /* temp workaround for old data */ else
-    (if(id.contains("-")) id.substring(0,id.indexOf('-')) else id) + "/" + id.replace(".xml", ".png"))
+    (if (id.matches(".*?PPN345572629_0004-000[1-6].*?")) id /* temp workaround for old data */ else
+      (if (id.contains("-")) id.substring(0, id.indexOf('-')) else id) + "/" + id.replace(".xml", ".png"))
 
   def user(id: String) = {
-    val user = User.fromXml(db.getXml(col+"/users", id + ".xml").get(0))
+    val user = User.fromXml(db.getXml(col + "/users", id + ".xml").get(0))
     val link = imageLink(user.latestPage)
     val page = new Page(null, user.latestPage)
     Template(user, link, page)
@@ -42,7 +53,7 @@ object Application extends Controller {
       "@signup".asTemplate
     } else {
       val u = User(id, name, region, pass, col, db)
-      db.putXml(u.toXml, col+"/users", id + ".xml")
+      db.putXml(u.toXml, col + "/users", id + ".xml")
       Action(user(id))
     }
   }
