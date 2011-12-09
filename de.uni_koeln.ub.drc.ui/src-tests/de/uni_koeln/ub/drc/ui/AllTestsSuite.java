c658f8d7595e11f80fa9eebc45e72235ffff126c
Fabian Steeg <fsteeg@gmail.com>
2011-12-09 18:10:05 +0100

    Basic UI test setup with WindowTester (closes #185)

--- de.uni_koeln.ub.drc.ui/src-tests/de/uni_koeln/ub/drc/ui/AllTestsSuite.java
+++ de.uni_koeln.ub.drc.ui/src-tests/de/uni_koeln/ub/drc/ui/AllTestsSuite.java
@@ -1,5 +1,5 @@
 /*************************************************************************************************
- * Copyright (c) 2010 Fabian Steeg. All rights reserved. This program and the accompanying materials
+ * Copyright (c) 2011 Fabian Steeg. All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  * <p/>
@@ -16,20 +16,18 @@ import org.junit.runners.Suite;
 
 /**
  * Main test suite.
+ * 
  * @author Fabian Steeg (fsteeg)
  */
-@RunWith( Suite.class )
-@Suite.SuiteClasses( { TestDrcHeadless.class, TestDrcUi.class  } )
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ SearchEditorTest.class, ViewsEditorTest.class })
 public final class AllTestsSuite {
 
-  static final String APPLICATION_XMI = "de.uni_koeln.ub.drc.ui/Application.e4xmi";
-  static final String[] PART_NAMES = { "SearchView", "EditView", "CheckView", "WordView" };
-
-  /** Check if we are running as JUnit Plug-in test. */
-  @Before
-  public void setup() {
-    if (!Platform.isRunning()) {
-      Assert.fail("Please run as JUnit Plug-in test");
-    }
-  }
+	/** Check if we are running as JUnit Plug-in test. */
+	@Before
+	public void setup() {
+		if (!Platform.isRunning()) {
+			Assert.fail("Please run as JUnit Plug-in test");
+		}
+	}
 }
