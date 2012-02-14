19e5a298afe4c83b5d800dee05c9c2ae2b0b84f0
Claes <c.neuefeind@uni-koeln.de>
2012-02-14 16:25:03 +0100

    Changed db location

--- de.uni_koeln.ub.drc/src/scala/de/uni_koeln/ub/drc/data/Index.scala
+++ de.uni_koeln.ub.drc/src/scala/de/uni_koeln/ub/drc/data/Index.scala
@@ -91,8 +91,8 @@ object SearchOption extends Enumeration {
 object Import extends Application {
 
   //private val db = Index.LocalDb
-  //val db = XmlDb("bob.spinfo.uni-koeln.de", 7070)
-  val db = XmlDb("hydra1.spinfo.uni-koeln.de", 8080)
+  val db = XmlDb("bob.spinfo.uni-koeln.de", 7070)
+  //val db = XmlDb("hydra1.spinfo.uni-koeln.de", 8080)
 
   val folder = "res/rom/"
   val prefix = "PPN345572629_"
